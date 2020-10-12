63cced8647ee7e21896e601b020fda8c6d0067f2
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-12 14:31:28 +0300

    Add users listing and style views  using bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end 
+
   def new
     @user = User.new
   end
@@ -7,7 +11,7 @@ class UsersController < ApplicationController
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to root_path
     else
       render :new
     end
@@ -20,7 +24,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to new_user_path
+      redirect_to root_path
     else
       render :edit
     end
